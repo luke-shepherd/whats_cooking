969a52d618a289d6904be676c1202404d71b3737
Luke Shepherd <lukeshep@gmail.com>
2017-03-22 00:18:58 -0700

    real bad softmax classifier

--- whats_cooking/softMax.py
+++ whats_cooking/softMax.py
@@ -1,13 +1,12 @@
 import numpy as np
 from dataParser import parse_input
-
+import random
 # most of this code is written by Arthur Juliani
 # https://medium.com/@awjuliani/simple-softmax-in-python-tutorial-d6b4c4ed5c16#.xpni65dgy
 
 def getLoss(w,x,y,lam):
     m = x.shape[0] #First we get the number of training examples
-    
-    
+     
     scores = np.dot(x,w) #Then we compute raw class scores given our input and current weights
     prob = softmax(scores) #Next we perform a softmax on these scores to get their probabilities
     loss = (-1 / m) * np.sum(y * np.log(prob)) + (lam/2)*np.sum(w*w) #We then find the loss of the probabilities
@@ -20,23 +19,64 @@ def softmax(z):
     sm = (np.exp(z).T / np.sum(np.exp(z),axis=1)).T
     return sm
 
-def getProbsAndPreds(someX):
+def getProbsAndPreds(someX, w):
     probs = softmax(np.dot(someX,w))
     preds = np.argmax(probs,axis=1)
+    #print 'probs shape ', probs.shape
+    #print 'preds is ', preds.shape
     return probs,preds
 
 
+def getAccuracy(someX,someY, w):
+    prob,prede = getProbsAndPreds(someX,w)
+    accuracy = sum(prede == someY)/(float(len(someY)))
+    return accuracy
 
+
+print 'Fetching inputs...'
 (all_classes, ingredients, X, y) = parse_input('train.json')
 print 'Loaded inputs'
 
-w = np.zeros([X.shape[1], y.shape[1])
+X_train = []
+X_test = []
+y_train = []
+y_test = []
+for i in range(0, X.shape[0]):
+    r = random.random()
+    if r < .75:
+        X_train.append(X[i, :])
+        y_train.append(y[i, :])
+    else:
+        X_test.append(X[i, :])
+        y_test.append(y[i, :])
+
+X_train = np.array(X_train)
+X_test = np.array(X_test)
+y_train = np.array(y_train)
+y_test = np.array(y_test)
+
+print 'split data'
+
+print 'train loop'
+w = np.zeros([X_train.shape[1], y_train.shape[1]])
 lam = 1
 iterations = 1000
-learningRate = 1e-5
+learningRate = .002
 losses = []
 for i in range(0,iterations):
-    loss,grad = getLoss(w,X,y,lam)
-    print 'loss at iteration ',i,': ',loss
+    loss,grad = getLoss(w,X_train,y_train,lam)
+    print 'loss at iteration ',i,': ',loss     
     losses.append(loss)
     w = w - (learningRate * grad)
+
+
+y_train_labels = []
+y_test_labels = []
+print 'formatting y'
+for row in y_train:
+    x,y = np.where(y_train==1)
+    y_train_labels.append(y[0])
+    
+    
+print 'Training error: ', getAccuracy(X_train, y_train_labels, w)
+#print 'Testing error: ', getAccuracy(X_test, y_test, w)
