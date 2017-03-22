2322c7409cf72e6c13675f101fa196a51e244697
Luke Shepherd <lukeshep@gmail.com>
2017-03-21 19:33:46 -0700

    create X vector

--- whats_cooking/linearClassify.py
+++ whats_cooking/linearClassify.py
@@ -12,8 +12,6 @@ def parse_input(filename):
 	# data structures
         all_classes = [] # target values
         all_ingredients = [] # all features
-        ing_indexes = {} # maintain column index of features
-        curr_index = 1 # counter for feature columne indices
         NUM_EXAMPLES = len(data)
 
 	# iterate over the cuisines
@@ -30,35 +28,46 @@ def parse_input(filename):
 		# add new ingredient to list
                 if ingredient not in all_ingredients:
                     all_ingredients.append(ingredient)
-                    ing_indexes[ingredient] = curr_index
-                    curr_index += 1
         
         # create class column vector
         all_classes = np.array(all_classes)
         all_classes = np.resize(all_classes, (all_classes.size, 1))
+        
+        
         print 'There are ', all_classes.shape, ' classes\n' 
         print all_classes
 
-        all_ingredients = np.array(all_ingredients)
-        all_ingredients = np.resize(all_ingredients, (all_ingredients.size, 1))
-        print 'There are ', all_ingredients.size, ' different ingredients\n'
-        print all_ingredients
+        ingredients = np.array(all_ingredients)
+        ingredients = np.resize(ingredients, (ingredients.size, 1))
+        print 'There are ', ingredients.size, ' different ingredients\n'
+        print ingredients
 
 
         print '\n Dims of ingredients'
-        print all_ingredients.shape
+        print ingredients.shape
 
-        NUM_ING = all_ingredients.size
+        
+        NUM_ING = ingredients.size
         train_rows = []
         y_rows = []
         
         for training_example in data:
-            ing = training_example['cuisine']
-            row = np.zeros((1, NUM_ING))
+            ing = training_example['ingredients']
+            f_vec = np.zeros((1, NUM_ING))
             for ingredient in ing:
-                
+                np.put(f_vec, all_ingredients.index(ingredient), 1)
+            train_rows.append(f_vec)
 
+            y_rows.append(training_example['cuisine'])
+
+        X = np.array(train_rows)
+        y = np.array(y_rows)
+
+        print 'X'
+        print X
+
+        return (all_classes, ingredients, X, y)
              
 
-(classes, X_train) = parse_input('train.json')
+(classes, ingredients, X_train, y_train) = parse_input('train.json')
 
